15baf3fc5e731319c53bc340752f04f34e27dc9b
Lorenzo Calamita <calaml@ititcalamlm3.local>
2016-03-21 13:13:00 +0100

    Initial Commit

---
+++ iBaby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBaby
+//
+//  Created by Calamita, Lorenzo on 21/03/16.
+//  Copyright © 2016 Calamita, Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
