e9816fd370450ae2bceff7789b3d94aa345ee518
lorenzo <calamita.lorenzo@gmail.com>
2016-06-30 00:20:01 +0200

    pulling weekly plan from service

--- iBaby/AppDelegate.swift
+++ iBaby/AppDelegate.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, NSURLSessionDelegate {
 
     var window: UIWindow?
-
+    var dataJ = NSMutableData()
+    var myResults : NSArray = []
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
         let defaults = NSUserDefaults.standardUserDefaults()
+         downloadItems()
+        let defaultWeek = myResults
+        defaults.setObject (defaultWeek, forKey: "defaultWeeklySchedule")
         //defaults.setBool(true, forKey: "payed")
         //defaults.setBool(true, forKey: "thisMonth")
         //let defaultWeek: [String:String] = ["LUN":"Tatiana","MAR":"Chiara","MER":"Tatiana","GIO":"Free","FRI":"Chiara" ]
@@ -29,18 +33,76 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         
         if !defaults.boolForKey("firstLaunch") {
+           
             defaults.setInteger(40, forKey: "Age")
             defaults.setBool(true, forKey: "firstLaunch")
             defaults.setObject(["Tatiana","Chiara"], forKey: "BS")
             defaults.setFloat(8.00, forKey: "hourlySalary")
-            let defaultWeek: [String:String] = ["LUN":"Chiara","MAR":"Chiara","MER":"Chiara","GIO":"Chiara","FRI":"Chiara" ]
-            defaults.setObject (defaultWeek, forKey: "defaultWeeklySchedule")
+
 
            
         }
 
         return true
     }
+
+    
+    
+    func downloadItems() {
+        //downloadingIndicator.startAnimating()
+        let url: NSURL = NSURL(string: "http://www.cuttons.com/json/plan.php")!
+        //let url: NSURL = NSURL(string: "http://itwine.corp.emc.com")!
+        var session: NSURLSession!
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        session = NSURLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+        
+        let task = session.dataTaskWithURL(url)
+        
+        task.resume()
+        
+    }
+    
+    
+    func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData data: NSData) {
+        self.dataJ.appendData(data);
+        
+    }
+    
+    
+    func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+        
+        if error != nil {
+            print("Failed to download weekly plan data")
+        } else {
+            print("Data downloaded")
+            let dataString = String(data: dataJ, encoding: NSUTF8StringEncoding)
+            print("\(dataString)")
+            myResults = parseJSON()
+            let defaults = NSUserDefaults.standardUserDefaults()
+            defaults.setObject (myResults, forKey: "defaultWeeklySchedule")
+        }
+            
+    }
+        
+
+    
+    
+    func parseJSON() -> NSArray {
+        
+        var json : NSArray = []
+        
+        do {
+            json = try NSJSONSerialization.JSONObjectWithData (self.dataJ, options:NSJSONReadingOptions.AllowFragments) as! NSArray
+        } catch let error as NSError {
+            print(error)
+        }
+        
+            print(json.count)
+            return json
+        
+    }
+
 
     
     func applicationWillResignActive(application: UIApplication) {
